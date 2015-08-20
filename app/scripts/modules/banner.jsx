0c75255a5da484ecc471fe865301ce35e071780c
Andrii Pylat <pilatao91@gmail.com>
2015-08-20 12:46:15 +0300

    #js10 created banner module layout. Menu styles improvement

--- app/scripts/modules/banner.jsx
+++ app/scripts/modules/banner.jsx
@@ -3,10 +3,9 @@ var React = require('react');
 
 var Banner = React.createClass({
   render: function() {
-    return <div>
-    I am Banner
-      <div>And here is my title: {this.props.initData.data.title}</div>
-    </div>;
+    return (
+      <section className="timeline"></section>
+    );
   }
 });
 
